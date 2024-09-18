3b9c7ac6a912c0e5d9e5b5131781d7fc6d80d4a1
Zishan Shaikh <zishan@truelink.ai/in>
2024-09-18 15:10:29 +0530

    dependencies added

--- src/env.ts
+++ src/env.ts
@@ -1,5 +1,5 @@
 export const vars = {
-	MONGODB_URI: process.env.MONGODB_URI!!,
+	MONGODB_URI: process.env.MONGODB_URI || 'mongodb://localhost:27017',
 	ACCESSTOKEN_SECRET: process.env.ACCESSTOKEN_SECRET || 'thisisasecret',
 	ACCESSTOKEN_EXPIRY: process.env.ACCESSTOKEN_EXPIRY || '1h',
 }
